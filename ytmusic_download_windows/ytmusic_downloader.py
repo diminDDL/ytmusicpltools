85617f3f5788e27e9674fb86f823fe3b9be3cbfe
diminDDL <dimin21590@gmail.com>
2022-05-28 00:08:58 +0200

    cleaned the code up a bit

--- ytmusic_download_windows/ytmusic_downloader.py
+++ ytmusic_download_windows/ytmusic_downloader.py
@@ -8,10 +8,6 @@ songs = ytmusic.get_liked_songs(limit=5000)
 f = open("IdList.txt", "r")
 IdList = f.read()
 f.close()
-#print("the downloaded Ids will be listed below:")
-#print(IdList)
-#print(type(IdList))
-#print(os.path.abspath("youtube-dl.exe"))
 
 q = len(songs['tracks'])
 q -= 1
@@ -30,17 +26,3 @@ while(q >= 0):
         f.write(IdList)
         f.truncate()
     q -= 1
-
-# for n in songs['tracks']:
-#     print("Current video Id: " + n['videoId'])
-#     #print("Current track Id: " +  n[''])
-#     if n['videoId'] not in IdList:
-#         print("Not downloaded previously, downloading...")
-#         IdList += n['videoId']
-#         IdList += "\n"
-#         os.system('{} --extract-audio --audio-format mp3 --audio-quality 0 --embed-thumbnail --add-metadata --metadata-from-title "(?P<artist>.+?) - (?P<title>.+)" --output "dl\%(creator)s - %(title)s.%(ext)s" https://www.youtube.com/watch?v={}'.format(os.path.abspath("youtube-dl.exe"),n['videoId']))
-#         time.sleep(1)
-#         f = open("IdList.txt", "w")
-#         f.seek(0)
-#         f.write(IdList)
-#         f.truncate()
